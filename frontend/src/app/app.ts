c0708309c8988a874a45aab0d87d80d692dc97e0
Joao Victor Otto <ottojvn@tuta.io>
2026-01-22 15:21:42 -0300

    feat: usa task service para exibir tasks no frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,30 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { TaskService } from './services/task.service';
+import { Task } from './models/task.model';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('frontend');
+
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe(
+      (data) => {
+        console.log('Tasks received:', data);
+        this.tasks = data;
+      },
+      (error) => console.error('Error:', error)
+    );
+  }
 }
