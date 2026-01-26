05f9ce7059315175170564f0b95318dd6e392d17
Joao Victor Otto <ottojvn@tuta.io>
2026-01-26 11:07:04 -0300

    feat: add find by status and by id to TaskRepository.java

--- backend/src/main/java/com/taskflow/repository/TaskRepository.java
+++ backend/src/main/java/com/taskflow/repository/TaskRepository.java
@@ -1,11 +1,18 @@
 package com.taskflow.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.taskflow.entity.Status;
 import com.taskflow.entity.Task;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    public Page<Task> findByStatus(Status status, Pageable pageable);
 
+    public Optional<Task> findById(Long id);
 }
